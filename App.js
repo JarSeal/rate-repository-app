ebce26cbff9f5c09a7771ae32abe3ddaaaf6dad9
JarSeal <kai.forsman@gmail.com>
2020-12-14 18:55:53 +0800

    Added and configured eslint.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app, dudeströmbergi fdjklsa fjkdsa jfkldsa jlfkdsa!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,6 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingLeft: 20
   },
 });
